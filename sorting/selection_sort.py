1208d9308dc6704e3c700e772ef50cbb28b2fbcc
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-08-16 18:05:50 +0300

    add selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,13 +1,24 @@
-import traceback
-import sys
+from helper import swap
 
 __author__ = 'Ivan Pobeguts'
 
-if __name__ == '__main__':
-    try:
-        pass
-    except:
-        (ErrorType, ErrorValue, ErrorTB) = sys.exc_info()
-        print('*** ERROR: type {} value {}'.format(ErrorType, ErrorValue))
-        traceback.print_tb(ErrorTB)
 
+def selection_sort(list_to_sort):
+    step = 0
+    for i in range(0, len(list_to_sort)):
+        min = i
+        for j in range(i + 1, len(list_to_sort)):
+            if list_to_sort[j] < list_to_sort[min]:
+                min = j
+        swap(list_to_sort, min, i)
+        print('Step {}: {}'.format(step, list_to_sort))
+        step += 1
+
+
+if __name__ == '__main__':
+    list_to_sort = [3, 7, 4, 4, 6, 5, 8, 2, 10, 98, 9, 24, 1]
+    print('Selection sort')
+    print('-' * 50)
+    print('Unsorted list: ', list_to_sort)
+    selection_sort(list_to_sort)
+    print('Sorted list: ', list_to_sort)
